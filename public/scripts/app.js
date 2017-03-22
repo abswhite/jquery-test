991d3a2091fe9b6cefedbd4dcd8a478ae4c570f3
Abigail White <abigailwhite213@gmail.com>
2017-03-22 13:54:52 -0400

    basic functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,26 @@
 //           $(this).addClass('red');
 // });
 
-$('h1').click(function(){
+//Change Color on Button Click
+$('#color-change').click(function(){
     $('h1').toggleClass('red');
 });
+
+//Change font on Button click
+$('#font-change').click(function() {
+  $('h1').toggleClass('font');
+});
+
+//Change background color
+$('#background-change').click(function() {
+  $('h1').toggleClass('background');
+});
+
+//Change input value
+$('input')
+  .keyup(function() {
+    let value = $(this).val();
+    $('h1').text(value);
+  })
+
+  .keyup();
