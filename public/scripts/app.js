8cc765c70574e1843c83e5725ce866c5fb11a6c9
Abigail White <abigailwhite213@gmail.com>
2017-03-22 14:25:31 -0400

    random generator

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,31 @@
 'use strict';
 
-// $('h1').click(function(){
-//     if(!$(this).hasClass('red'))
-//           $(this).addClass('red');
-// });
-
 //Change Color on Button Click
 $('#color-change').click(function(){
-    $('h1').toggleClass('red');
+    let colors = ['#8B8989', '#CD5555', '#8C1717', '#EE3B3B', '#FF6347', '##8B5742', '#FFF5EE', '#CD8500', '#FFCC11', '#777733'];
+    let random = colors[Math.floor(Math.random() * colors.length)];
+    $('h1').css('color',random);
+    $('#color-change').css('color', random);
 });
 
 //Change font on Button click
 $('#font-change').click(function() {
-  $('h1').toggleClass('font');
+  let font = ['Oswald','Syncopate','Marvel', 'Rubik Mono One', 'Quantico', 'Wire One', 'Fascinate Inline', 'Monoton', 'Faster One']
+  let random = font[Math.floor(Math.random() * font.length)];
+  $('h1').css('font-family', random);
+  $('#font-change').css('font-family', random);
 });
 
 //Change background color
 $('#background-change').click(function() {
-  $('h1').toggleClass('background');
+  let colors = ['#8B8989', '#CD5555', '#8C1717', '#EE3B3B', '#FF6347', '##8B5742', '#FFF5EE', '#CD8500', '#FFCC11', '#777733'];
+  let random = colors[Math.floor(Math.random() * colors.length)];
+  $('h1').css('background',random);
+  $('#background-change').css('background',random);
 });
 
 //Change input value
-$('input')
+$('textarea')
   .keyup(function() {
     let value = $(this).val();
     $('h1').text(value);
